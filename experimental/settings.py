587f793cefe5bb3db855f6090e599e0bc272e5a2
Popescu Manuel Denis <popescu.manuel.denis@gmail.com>
2023-01-16 12:02:01 +0200

    Update settings.py

--- experimental/settings.py
+++ experimental/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m12r-tziw-zsbe)_ut^+#z_yqx^3sdu7iy0bk4-w7q*k40fs5v'
+SECRET_KEY = 'django-insecure-m12r-tziw-zsbe)_ut^+#z_yqx^3sdu7iy0bk4-w7q*k40fs5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
